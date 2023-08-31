afb0fbabc57aebb2c4628fe9f4f8fa64f65af6e0
DESKTOP-13RQSSJ\HP <Ezechukwuchirimmadu@gmail.com>
2023-08-31 11:12:29 +0100

    feat: write more tests for middlewares

--- test/middlewares/middlewares.test.ts
+++ test/middlewares/middlewares.test.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+
 import {   
     validateCreateUserBody, 
     validateSignInUserBody, 
@@ -7,13 +8,15 @@ import {
     validateParamId,
     validateByDayQuery
 } from "../../src/middlewares/bodyValidators";
-import { BadRequestError } from "../../src/errors/errors";
+import { BadRequestError, UnauthorizedError } from "../../src/errors/errors";
+import {authenticator} from '../../src/middlewares/authenticator'
+import { signJwt, verifyJwt } from "../../src/utils/jwtUtils";
 
 describe('middleware test suites', ()=>{
 
     let userRequestStub : any
     let responseStub : Response
-    let mockNextStub = jest.fn()
+    let mockNextFunction = jest.fn()
     let correctUser =  {
         "firstName": "victor",
         "lastName": "ifaenyi",
@@ -26,7 +29,7 @@ describe('middleware test suites', ()=>{
     } 
 
     beforeEach(()=>{
-        mockNextStub.mockReset()
+        mockNextFunction.mockReset()
         userRequestStub  = {
             params:{
                 id : '001'
@@ -36,6 +39,12 @@ describe('middleware test suites', ()=>{
             },
             query : {
                 dayOfWeek : 'sunda'
+            },
+            headers : {
+                authorization : ''
+            },
+            user : {
+
             }
         }
     })
@@ -43,78 +52,109 @@ describe('middleware test suites', ()=>{
     it('should throw error if create user body is not right', ()=>{
         expect.assertions(2)
         try {
-            validateCreateUserBody(userRequestStub, responseStub, mockNextStub)
+            validateCreateUserBody(userRequestStub, responseStub, mockNextFunction)
         } catch (error) {
             expect(error).toBeInstanceOf(BadRequestError)
         }
 
         userRequestStub.body = correctUser
-        validateCreateUserBody(userRequestStub, responseStub, mockNextStub)
-        expect(mockNextStub).toHaveBeenCalledTimes(1)
+        validateCreateUserBody(userRequestStub, responseStub, mockNextFunction)
+        expect(mockNextFunction).toHaveBeenCalledTimes(1)
     })
 
     it('should throw error if signIn user body is not right', ()=>{
         expect.assertions(2)
         try {
-            validateSignInUserBody(userRequestStub, responseStub, mockNextStub)
+            validateSignInUserBody(userRequestStub, responseStub, mockNextFunction)
         } catch (error) {
             expect(error).toBeInstanceOf(BadRequestError)
         }
 
         userRequestStub.body = {'email' : 'ovi@gmail.com', 'password' : '123456'}
-        validateSignInUserBody(userRequestStub, responseStub, mockNextStub)
-        expect(mockNextStub).toHaveBeenCalledTimes(1)
+        validateSignInUserBody(userRequestStub, responseStub, mockNextFunction)
+        expect(mockNextFunction).toHaveBeenCalledTimes(1)
     })
 
     it('should throw error if event to be created does not have correct structure', ()=>{
         expect.assertions(2)
         try {
-            validateEventToBeCreated(userRequestStub, responseStub, mockNextStub)
+            validateEventToBeCreated(userRequestStub, responseStub, mockNextFunction)
         } catch (error) {
             expect(error).toBeInstanceOf(BadRequestError)
         }
 
         userRequestStub.body = {dayOfWeek : 'sunday', description : 'test'}
-        validateEventToBeCreated(userRequestStub, responseStub, mockNextStub)
-        expect(mockNextStub).toHaveBeenCalledTimes(1)
+        validateEventToBeCreated(userRequestStub, responseStub, mockNextFunction)
+        expect(mockNextFunction).toHaveBeenCalledTimes(1)
     })
 
     it('should throw error if event query does not have correct structure', ()=>{
         expect.assertions(2)
         try {
-            validateEventByDesorDayQuery(userRequestStub, responseStub, mockNextStub)
+            validateEventByDesorDayQuery(userRequestStub, responseStub, mockNextFunction)
         } catch (error) {
             expect(error).toBeInstanceOf(BadRequestError)
         }
 
         userRequestStub.query.dayOfWeek = 'sunday'
-        validateEventByDesorDayQuery(userRequestStub, responseStub, mockNextStub) 
-        expect(mockNextStub).toHaveBeenCalledTimes(1)
+        validateEventByDesorDayQuery(userRequestStub, responseStub, mockNextFunction) 
+        expect(mockNextFunction).toHaveBeenCalledTimes(1)
     })
 
     it('should throw error if req.param id is not correct', ()=>{
         expect.assertions(2)
         try {
-            validateParamId(userRequestStub, responseStub, mockNextStub)
+            validateParamId(userRequestStub, responseStub, mockNextFunction)
         } catch (error) {
             expect(error).toBeInstanceOf(BadRequestError)
         }
 
         userRequestStub.params.id = '64ef2eb9823d6b90e7a82ab3'
-        validateParamId(userRequestStub, responseStub, mockNextStub)
-        expect(mockNextStub).toHaveBeenCalledTimes(1)
+        validateParamId(userRequestStub, responseStub, mockNextFunction)
+        expect(mockNextFunction).toHaveBeenCalledTimes(1)
     })
 
     it('should ensure that by day query is a valid day', () => {
         expect.assertions(2)
         try {
-            validateByDayQuery(userRequestStub, responseStub, mockNextStub)
+            validateByDayQuery(userRequestStub, responseStub, mockNextFunction)
         } catch (error) {
             expect(error).toBeInstanceOf(BadRequestError)
         }
 
         userRequestStub.query = {dayOfWeek : 'sunday'}
-        validateByDayQuery(userRequestStub, responseStub, mockNextStub)
-        expect(mockNextStub).toHaveBeenCalledTimes(1)
+        validateByDayQuery(userRequestStub, responseStub, mockNextFunction)
+        expect(mockNextFunction).toHaveBeenCalledTimes(1)
+    })
+
+    it('should authenticate all requests to secured routes', ()=>{
+        expect.assertions(5)
+        try {
+            authenticator(userRequestStub, responseStub, mockNextFunction)
+        } catch (error) {
+            expect(error).toBeInstanceOf(UnauthorizedError)
+        }
+
+        try {
+            userRequestStub.headers.authorization = 'Bearer'
+            authenticator(userRequestStub, responseStub, mockNextFunction) 
+        } catch (error) {
+            expect(error).toBeInstanceOf(UnauthorizedError)
+        }
+
+        
+        try {
+            const inValidToken = signJwt({wrongKey : 'invalidString'})
+            userRequestStub.headers.authorization = 'Bearer ' + inValidToken
+            authenticator(userRequestStub, responseStub, mockNextFunction)
+        } catch (error) {
+            expect(error).toBeInstanceOf(UnauthorizedError)
+        }
+        
+        const ValidToken = signJwt({userId : 'validID'})
+        userRequestStub.headers.authorization = 'Bearer ' + ValidToken
+        authenticator(userRequestStub, responseStub, mockNextFunction)
+        expect(userRequestStub.user).toHaveProperty('userId', 'validID') 
+        expect(mockNextFunction).toBeCalledTimes(1)
     })
 })
